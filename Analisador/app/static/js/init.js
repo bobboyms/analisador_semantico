a23ee12bbdacc3c1f2c9a84f2fd0f20157b8ec28
Gustavo Franco <gustavofrancojp@gmail.com>
2018-11-01 16:02:52 -0400

    Ajuste de Layout

--- Analisador/app/static/js/init.js
+++ Analisador/app/static/js/init.js
@@ -11,3 +11,14 @@ $(document).ready(function(){
   $(".collapsible-header").addClass("active"); //manter campos abertos ao iniciar
   $('.collapsible').collapsible({accordion: false}); // accordion serve para não deixar um campo fechar ao abrir outro
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
